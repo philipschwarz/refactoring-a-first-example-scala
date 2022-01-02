997195a52e6b26b908867c662f92e0fd2fe9d91b
philipschwarz <philip.johann.schwarz@gmail.com>
2022-01-02 11:55:39 +0100

    Get rid of remaining mutability in the rendering logic.

--- src/main/scala/Statement.scala
+++ src/main/scala/Statement.scala
@@ -5,29 +5,32 @@ def statement(invoice: Invoice, plays: Map[String, Play]): String =
   renderPlainText(createStatementData(invoice,plays))
 
 def renderPlainText(data: StatementData): String =
-
-  var result = s"Statement for ${data.customer}\n"
-  for (perf <- data.performances)
-    result += s"  ${perf.play.name}: ${usd(perf.amount/100)} (${perf.audience} seats)\n"
-
-  result += s"Amount owed is ${usd(data.totalAmount/100)}\n"
-  result += s"You earned ${data.totalVolumeCredits} credits\n"
-  result
+  s"Statement for ${data.customer}\n" + (
+    for
+      perf <- data.performances
+    yield s"  ${perf.play.name}: ${usd(perf.amount/100)} (${perf.audience} seats)\n"
+  ).mkString +
+  s"""|Amount owed is ${usd(data.totalAmount/100)}
+      |You earned ${data.totalVolumeCredits} credits
+      |""".stripMargin
 
 def htmlStatement(invoice: Invoice, plays: Map[String, Play]): String =
   renderHtml(createStatementData(invoice,plays))
 
 def renderHtml(data: StatementData): String =
-  var result = s"<h1>Statement for ${data.customer}</h1>\n"
-  result += "<table>\n"
-  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n"
-  for (perf <- data.performances)
-    result += s"<tr><td>${perf.play.name}</td><td>${perf.audience}</td>"
-    result += s"<td>${usd(perf.amount/100)}</td></tr>\n"
-  result += "</table>\n"
-  result += s"<p>Amount owed is <em>${usd(data.totalAmount/100)}</em></p>\n"
-  result += s"<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n"
-  result
+  s"""|<h1>Statement for ${data.customer}</h1>
+      |<table>
+      |<tr><th>play</th><th>seats</th><th>cost</th></tr>
+      |""".stripMargin + (
+    for
+      perf <- data.performances
+    yield  s"<tr><td>${perf.play.name}</td><td>${perf.audience}</td>" +
+           s"<td>${usd(perf.amount/100)}</td></tr>\n"
+  ).mkString +
+  s"""|</table>
+      |<p>Amount owed is <em>${usd(data.totalAmount/100)}</em></p>
+      |<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>
+      |""".stripMargin
 
 def usd(aNumber: Int): String =
   val formatter = NumberFormat.getCurrencyInstance(Locale.US)
