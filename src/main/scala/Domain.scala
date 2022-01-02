6ea2a78d4132a068e59b51748c98a5bac784fd60
philipschwarz <philip.johann.schwarz@gmail.com>
2022-01-02 11:32:11 +0100

    Replace Conditional with Polymorphism - move down the comedy logic for amount

--- src/main/scala/Domain.scala
+++ src/main/scala/Domain.scala
@@ -20,30 +20,24 @@ case class StatementData(
 sealed trait PerformanceCalculator:
   def performance: Performance
   def play: Play
-  def amount: Int =
-    var result = 0
-    play.`type` match
-      case "tragedy" => throw IllegalArgumentException("bad thing")
-      case "comedy" =>
-        result = 30_000
-        if performance.audience > 20
-        then result += 10_000 + 500 * (performance.audience - 20)
-        result += 300 * performance.audience
-      case other =>
-        throw IllegalArgumentException(s"unknown type ${play.`type`}")
-    result
+  def amount: Int
   def volumeCredits: Int =
     var result = 0
     result += math.max(performance.audience - 30, 0)
     if "comedy" == play.`type` then result += math.floor(performance.audience / 5).toInt
     result
 case class TragedyCalculator(performance: Performance, play: Play) extends PerformanceCalculator:
-  override def amount: Int =
+  def amount: Int =
     var result = 0
     result = 40_000
     if performance.audience > 30 then result += 1_000 * (performance.audience - 30)
     result
-case class ComedyCalculator(performance: Performance, play: Play) extends PerformanceCalculator
+case class ComedyCalculator(performance: Performance, play: Play) extends PerformanceCalculator:
+  def amount: Int =
+    var result = 30_000
+    if performance.audience > 20 then result += 10_000 + 500 * (performance.audience - 20)
+    result += 300 * performance.audience
+    result
 object PerformanceCalculator:
   def apply(aPerformance: Performance, aPlay: Play): PerformanceCalculator =
     aPlay.`type` match
