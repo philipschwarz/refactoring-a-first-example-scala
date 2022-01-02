9365b45d394a4fe03bd3ea291b41c67ec274e17d
philipschwarz <philip.johann.schwarz@gmail.com>
2022-01-02 11:11:31 +0100

    get volumeCreditsFor to delegate to PerformanceCalculator.volumeCredits

--- src/main/scala/CreateStatementData.scala
+++ src/main/scala/CreateStatementData.scala
@@ -13,10 +13,7 @@ def createStatementData(invoice: Invoice, plays: Map[String,Play]): StatementDat
     plays(aPerformance.playID)
 
   def volumeCreditsFor(aPerformance: Performance): Int =
-    var result = 0
-    result += math.max(aPerformance.audience - 30, 0)
-    if "comedy" == playFor(aPerformance).`type` then result += math.floor(aPerformance.audience / 5).toInt
-    result
+    PerformanceCalculator(aPerformance,playFor(aPerformance)).volumeCredits
 
   def totalVolumeCredits(performances: List[EnrichedPerformance]): Int =
     performances.foldLeft(0)((total,perf) => total + perf.volumeCredits)
