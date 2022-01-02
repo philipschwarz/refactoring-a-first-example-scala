8d5649f6fd47892bf282d446b564aa380864cfa4
philipschwarz <philip.johann.schwarz@gmail.com>
2022-01-02 10:57:30 +0100

    Copy the logic of amountFor to a new  PerformanceCalculator function called amount, and adjust it to fit in its new home.

--- src/main/scala/Domain.scala
+++ src/main/scala/Domain.scala
@@ -17,4 +17,20 @@ case class StatementData(
   totalAmount: Int,
   totalVolumeCredits: Int)
 
-case class PerformanceCalculator(performance: Performance, play: Play)
+case class PerformanceCalculator(performance: Performance, play: Play):
+  def amount: Int =
+    var result = 0
+    play.`type` match
+      case "tragedy" =>
+        result = 40_000
+        if performance.audience > 30
+        then result += 1_000 * (performance.audience - 30)
+      case "comedy" =>
+        result = 30_000
+        if performance.audience > 20
+        then result += 10_000 + 500 * (performance.audience - 20)
+        result += 300 * performance.audience
+      case other =>
+        throw IllegalArgumentException(s"unknown type ${play.`type`}")
+    result
+  
